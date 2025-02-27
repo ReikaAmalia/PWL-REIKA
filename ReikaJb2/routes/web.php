9918b35c6e203d659489b74594a676c90ba4ba29
ReikaAmalia <reikaamalia17@gmail.com>
2025-02-27 17:21:29 +0700

    update

--- ReikaJb2/routes/web.php
+++ ReikaJb2/routes/web.php
@@ -49,6 +49,6 @@ Route::get('/articles/{id}', function ($Id) {
     return 'Halaman Artikel dengan ID '.$Id;
 });
 
-Route::get('/user/{name?}', function ($name='null') {
+Route::get('/user/{name?}', function ($name='John') {
     return 'Nama saya '.$name;
 });
