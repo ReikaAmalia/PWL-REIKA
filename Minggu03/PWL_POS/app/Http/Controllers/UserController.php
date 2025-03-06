183577f9c40e25e56ecb92d8b8455666686602db
ReikaAmalia <reikaamalia17@gmail.com>
2025-03-06 16:28:15 +0700

    update jb3

--- Minggu03/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu03/PWL_POS/app/Http/Controllers/UserController.php
@@ -2,12 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
-    public function show($id, $name)
+    // public function show($id, $name)
+    // {
+    //     return view('user', ['id' => $id, 'name' => $name]);
+    // }
+
+    // jb 3
+    // public function index()
+    // {
+    //     // coba akses model UserModel
+    //     $user = UserModel::all(); // ambil semua data dari tabel m_user
+    //     return view('user', ['data' => $user]);
+    // }
+
+    // tambah data user dengan Eloquent Model
+    // public function index()
+    // {
+    //     // Tambah data user dengan Eloquent Model
+    //     $data = [
+    //         'username' => 'customer-1',
+    //         'nama' => 'Pelanggan',
+    //         'password' => Hash::make('12345'),
+    //         'level_id' => 4
+    //     ];
+
+    //    // UserModel::insert($data); // Tambahkan data ke tabel m_user
+
+    //     // Ambil semua data dari tabel m_user
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
+
+    public function index()
     {
-        return view('user', ['id' => $id, 'name' => $name]);
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
-}
+};
--- Minggu03/PWL_POS/resources/views/user.blade.php
+++ Minggu03/PWL_POS/resources/views/user.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
 <head>
     <title>User Profile</title>
@@ -7,5 +7,32 @@
     <h1>Profil Pengguna</h1>
     <p>ID: {{ $id }}</p>
     <p>Nama: {{ $name }}</p>
+</body>
+</html> --}}
+
+{{-- jb 3 --}}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
+        </tr>
+        @foreach ($data as $d)
+        <tr>
+            <td>{{ $d->user_id }}</td>
+            <td>{{ $d->username }}</td>
+            <td>{{ $d->nama }}</td>
+            <td>{{ $d->level_id }}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 </html>
