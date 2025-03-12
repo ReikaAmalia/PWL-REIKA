c31e6c3d44a31b14d3c82d7bc32857253f875432
ReikaAmalia <reikaamalia17@gmail.com>
2025-03-12 14:23:31 +0700

    praktikum 2.1

--- Minggu04/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu04/PWL_POS/app/Http/Controllers/UserController.php
@@ -54,17 +54,43 @@ class UserController extends Controller
     //     return view('user', ['data' => $user]);
     // }
 
+    // public function index()
+    // {
+    //     $data = [
+    //         'level_id' => 2,
+    //         'username' => 'manager_tiga',
+    //         'nama' =>'Manager 3',
+    //         'password' => Hash::make('12345')
+    //     ];
+    //     UserModel::create($data);
+
+    //     $user = UserModel::all();
+    //     return view('user', ['data' => $user]);
+    // }
+
+    // public function index()
+    // {
+    //     $user = UserModel::find(1);
+    //     return view('user', ['data' => $user]);
+    // }
+
+    // public function index()
+    // {
+    //     $user = UserModel::where('level_id', 1)->first();
+    //     return view('user', ['data' => $user]);
+    // }
+
+    // public function index()
+    // {
+    //     $user = UserModel::firstWhere('level_id', 1);
+    //     return view('user', ['data' => $user]);
+    // }
+
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' =>'Manager 2',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
-
-        $user = UserModel::all();
+        $user = UserModel::findOr(20,['username', 'nama'], function (){
+            abort(404);
+        })->first();
         return view('user', ['data' => $user]);
     }
 };
