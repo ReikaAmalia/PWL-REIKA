8dda91ea1a33409af2d74a0b98a11b75810f9e5b
ReikaAmalia <reikaamalia17@gmail.com>
2025-03-18 18:38:16 +0700

    praktikum 3

--- Minggu05/PWL_POS/app/Http/Controllers/KategoriController.php
+++ Minggu05/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriModel;
 use App\DataTables\KategoriDataTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class KategoriController extends Controller
 {
@@ -26,5 +28,19 @@ class KategoriController extends Controller
 
         return $dataTable->render('kategori.index');
     }
-    
+
+        public function create()
+    {
+        return view('kategori.create');
+    }
+
+        public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+
+        return redirect('/kategori');
+    }
 }
