ebbcb87fbf28b3017d453ba2706a0f988f6599e3
ReikaAmalia <reikaamalia17@gmail.com>
2025-03-26 15:20:12 +0700

    TUGAS JB 5

--- Minggu05-02/PWL_POS/resources/views/layouts/sidebar.blade.php
+++ Minggu05-02/PWL_POS/resources/views/layouts/sidebar.blade.php
@@ -69,6 +69,13 @@
                 </a>
             </li>
 
+            <li class="nav-item">
+                <a href="{{ url('/supplier') }}" class="nav-link {{ $activeMenu == 'supplier' ? 'active' : '' }}">
+                    <i class="nav-icon fas fa-truck"></i>
+                    <p>Data Supplier</p>
+                </a>
+            </li>
+            
         </ul>
     </nav>
 </div>
