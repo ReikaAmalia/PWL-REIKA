b8f065c1539eb0c2a4456498faafe8b01da5a029
ReikaAmalia <reikaamalia17@gmail.com>
2025-04-08 12:20:59 +0700

    praktikum 1

--- Minggu07/PWL_POS/routes/web.php
+++ Minggu07/PWL_POS/routes/web.php
@@ -41,6 +41,7 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 
@@ -59,7 +60,19 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 //jb 5 terbaru prak 1
-Route::get('/', [WelcomeController::class, 'index']);
+// Route::get('/', [WelcomeController::class, 'index']);
+// jb 7
+Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
+ 
+ Route::get('login', [AuthController::class, 'login'])->name('login');
+ Route::post('login', [AuthController::class, 'postlogin']);
+ Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+ 
+ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+ 
+     // masukkan semua route yang perlu autentikasi di sini
+     Route::get('/', [WelcomeController::class, 'index']); 
+ });
 
 //jb 5 terbaru prak 2
 Route::group(['prefix' => 'user'], function () {
